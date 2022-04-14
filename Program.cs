3a45e222c8805723397289f0331007b36db0778f
heyuzhen <yuzhen.he@3dxt.com>
2022-04-14 16:58:40 +0800

    test1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// var fbd = new FolderBrowserDialog();
-// var rs = op.ShowDialog();
-// if (rs == DialogResult.OK)
-// {
-//     var path=op.SelectedPath;
-// }
 var p= args.First();
 var path = "sss";
+
+
 Environment.SetEnvironmentVariable("DIYHOMERUNTIME1", $"{path}", EnvironmentVariableTarget.Machine);
 Console.WriteLine(Environment.GetEnvironmentVariable("DIYHOMERUNTIME1"));
